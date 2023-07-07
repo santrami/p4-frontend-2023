eacea24b6310771bf7ea44541241d329041387f1
santrami <santrami@hotmail.com>
2023-07-07 20:34:14 +0200

    fixing object error (toString())

--- frontend-p4/src/components/Character.tsx
+++ frontend-p4/src/components/Character.tsx
@@ -1,9 +1,16 @@
+type Character = {
+  id: number;
+  name: string;
+  origin: string;
+  image: string;
+};
 
-
-
-
-export default function Character() {
+export default function Character({name, origin, image }: Character) {
   return (
-    <div>Character</div>
-  )
+    <div>
+      <img src={image.toString()} />
+      {name.toString()}
+      {origin.toString()}
+    </div>
+  );
 }
--- frontend-p4/src/components/Characters.tsx
+++ frontend-p4/src/components/Characters.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
+import Character from "./Character";
 
 type resultProps = {
   id: number;
   image: string;
   name: string;
+  origin: string;
 };
 
 type Pagination = {
@@ -29,7 +31,7 @@ function NavPage({ page, setPage }: Pagination) {
   );
 }
 
-export default function Character() {
+export default function Characters() {
   const [apiCall, setApiCall] = useState<resultProps[]>();
   const [page, setPage] = useState(1);
 
@@ -52,17 +54,17 @@ export default function Character() {
 
   return (
     <>
-      <div className="flex flex-row">
-        {
-          <h1>
-            {apiCall.map((char) => (
-              <div key={char.id.toString()}>
-                <img src={char.image} />
-                {char.name}
-              </div>
-            ))}
-          </h1>
-        }
+      <div className="flex flex-column">
+        {apiCall.map((char) => (
+          <div className="flex-1" key={char.id.toString()}>
+            <Character
+              id={char.id}
+              name={char.name}
+              origin={char.origin}
+              image={char.image}
+            />
+          </div>
+        ))}
       </div>
       <NavPage page={page} setPage={setPage} />
     </>
